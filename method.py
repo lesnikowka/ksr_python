c2ca963a13a5fa953d51341f20b9dc0f74a4886f
nik <lesnikowka@mail.ru>
2023-11-22 23:20:46 +0300

    method

--- method.py
+++ method.py
@@ -7,7 +7,7 @@ from scipy import integrate
 a = 0 #граничные условия
 b = 1 #граничные условия
 eps = 0.5e-6 #погрешность
-n = 1000 #число разбиений
+N = 100 #число разбиений
 
 
 #Тестовая Задача
@@ -93,13 +93,10 @@ def phiInit(ksi, xi, xmi, h, f1, f2):
 #Создание трёхдиагональной матрицы
 def matrixInit(k1, k2, q1, q2, f1, f2, mu1, mu2, ksi, n, a, b):
     h = (b - a) / n
-    A = [[0] * (n + 1) for i in range(n + 1)]
     Ai = [0] * (n) #Массивы коэфициентов для прогонки
     Bi = [0] * (n)
     Ci = [0] * (n)
     b = [0] * (n + 1)
-    A[0][0] = 1
-    A[n][n] = 1
     b[0] = mu1
     b[n] = mu2
     xi = 0
@@ -110,39 +107,37 @@ def matrixInit(k1, k2, q1, q2, f1, f2, mu1, mu2, ksi, n, a, b):
         xmi = xi - h #xi-1
         xmi2 = xi - h / 2 #xi-0.5
         b[i] = -phiInit(ksi, xi2, xmi2, h, f1, f2) #Столбец
-        for j in range(0, n + 1):
-            if i - j == 1:
-                A[i][j] = aiInit(ksi, xi, xmi, h, k1, k2) / h ** 2
-                Ai[i] = A[i][j]
-            if i == j:
-                A[i][j] = -((aiInit(ksi, xi, xmi, h, k1, k2) + aiInit(ksi, xi1, xi, h, k1, k2)) / h ** 2 + diInit(ksi, xi2, xmi2, h, q1, q2))
-                Ci[i] = -A[i][j]
-            if j - i == 1:
-                A[i][j] = aiInit(ksi, xi1, xi, h, k1, k2) / h ** 2
-                Bi[i] = A[i][j]
-    return A, b, Ai, Bi, Ci
+        for j in range(i - 1, i + 2):
+            if j == i - 1:
+                Ai[i] = aiInit(ksi, xi, xmi, h, k1, k2) / h ** 2
+            if j == i:
+                Ci[i] = ((aiInit(ksi, xi, xmi, h, k1, k2) + aiInit(ksi, xi1, xi, h, k1, k2)) / h ** 2 + diInit(ksi, xi2, xmi2, h, q1, q2))
+            if j == i + 1:
+                Bi[i] = aiInit(ksi, xi1, xi, h, k1, k2) / h ** 2
+    return b, Ai, Bi, Ci
 
 
-def progonka(matrix, b, Ai, Bi, Ci):
+def progonka(b, Ai, Bi, Ci, n):
     v = [0] * (n + 1)
     #прямой ход
     alpha = [0] * (n + 1)
     betta = [0] * (n + 1)
-    alpha[1] = matrix[0][1]
+    alpha[1] = 0
     betta[1] = b[0]
     for i in range(2, n + 1):
         alpha[i] = Bi[i - 1] / (Ci[i - 1] - Ai[i - 1] * alpha[i - 1])
         betta[i] = (-b[i - 1]  + Ai[i - 1] * betta[i - 1]) / (Ci[i - 1] - Ai[i - 1] * alpha[i - 1])
     #обратный ход
-    v[n] = (-matrix[n][n - 1] * betta[n] - b[n]) / (matrix[n][n - 1] * alpha[n] - 1)
+    v[n] = 0
     for i in range(n - 1, -1, -1):
         v[i] = alpha[i + 1] * v[i + 1] + betta[i + 1]
     return v
 
 #Тестовая задача ответ
-A, bs, Ai, Bi, Ci = matrixInit(k1test, k2test, q1test, q2test, f1test, f2test, 0, 0, 0.5, n, a, b)
-otvet = progonka(A, bs, Ai, Bi, Ci)
+bs, Ai, Bi, Ci = matrixInit(k1test, k2test, q1test, q2test, f1test, f2test, 0, 0, 0.5, N, a, b)
+otvet = progonka(bs, Ai, Bi, Ci, N)
 #print(otvet)
+
 
 #Аналитическое решение тестовой
 def u(x):
@@ -151,32 +146,29 @@ def u(x):
     elif x > 0.5 and x <= 1:
         return -0.110262 * np.exp(2 * x) - 1.36897 * np.exp(-(2 * x)) + 1
 
-xx = np.linspace(0, 1, n + 1)
-x = np.linspace(0, 1, n + 1)
+
+xx = np.linspace(0, 1, N + 1)
+x = np.linspace(0, 1, N + 1)
 plt.plot(xx, otvet)
 uarray = []
 
-for i in range(n + 1):
-    uarray.append(u(x[i]))
+for i in range(N + 1):
+    uarray.append(u(x[i])) 
 plt.plot(x, uarray)
 #print(uarray)
+plt.show()
+
 
 #Основная задача
-A, bs, Ai, Bi, Ci = matrixInit(k1, k2, q1, q2, f1, f2, 0, 0, 0.5, n, a, b)
-
-#for row in A:
-#    print(row)
-#print()
-
-otvet = progonka(A, bs, Ai, Bi, Ci)
-print(otvet)
-xx = np.linspace(0, 1, n + 1)
+m = 2 * N
+bs, Ai, Bi, Ci = matrixInit(k1, k2, q1, q2, f1, f2, 0, 0, 0.5, N, a, b)
+otvet = progonka(bs, Ai, Bi, Ci, N)
+#print(otvet)
+xx = np.linspace(0, 1, N + 1)
 plt.plot(xx, otvet)
 
-A, bs, Ai, Bi, Ci = matrixInit(k1, k2, q1, q2, f1, f2, 0, 0, 0.5, n, a, b)
-otvet = progonka(A, bs, Ai, Bi, Ci)
-print(otvet)
-xx = np.linspace(0, 1, n + 1)
-#plt.plot(xx, otvet)
-
-plt.show()
+bs, Ai, Bi, Ci = matrixInit(k1, k2, q1, q2, f1, f2, 0, 0, 0.5, m, a, b)
+otvet = progonka(bs, Ai, Bi, Ci, m)
+#print(otvet)
+xx = np.linspace(0, 1, m + 1)
+plt.plot(xx, otvet)
