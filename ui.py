fab0e278a41f3c3abda6eb1483620f8cd745968f
nik <lesnikowka@mail.ru>
2023-11-23 19:55:03 +0300

    connect method

--- ui.py
+++ ui.py
@@ -5,6 +5,7 @@ from matplotlib import pyplot as plt
 from PyQt5.QtGui import QPixmap
 import cv2
 import imutils
+import method
 
 class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
@@ -15,11 +16,11 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
 
         self.tableMain.setColumnCount(5)  # Set three columns
         self.tableMain.setRowCount(1)
-        self.tableMain.setHorizontalHeaderLabels(["Номер узла", "X", "V", "V*", "V-V*"])
+        self.tableMain.setHorizontalHeaderLabels(["Номер узла", "X", "V", "V*", "|V-V*|"])
 
         self.tableTest.setColumnCount(5)  # Set three columns
         self.tableTest.setRowCount(1)
-        self.tableTest.setHorizontalHeaderLabels(["Номер узла", "X", "U", "V", "U-V"])
+        self.tableTest.setHorizontalHeaderLabels(["Номер узла", "X", "U", "V", "|U-V|"])
 
     def resizeImage(self, imageName, height):
         image = cv2.imread(imageName)
@@ -28,7 +29,8 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         cv2.imwrite(imageName, resized)
 
     def drawMain(self):
-        graphName = "graphMain.png"
+        graph1Name = "graph1Main.png"
+        graph2Name = "graph2Main.png"
 
         N = 0
 
@@ -37,58 +39,63 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         except BaseException:
             return
 
-        x = [i for i in range(N)]
-        y = [i * i for i in x]
+        x, y, x2, y2 = method.calculate("main", N)
+        y2 = [y2[2 * i] for i in range(len(y))]
+        diff = [abs(y[i] - y2[i]) for i in range(len(y))]
 
         plt.plot(x, y)
-        plt.savefig(graphName)
+        plt.savefig(graph1Name)
+        plt.clf()
+
+        plt.plot(x, diff)
+        plt.savefig(graph2Name)
         plt.clf()
 
         size = self.graph1Main.size()
         height = size.height()
-
-        self.resizeImage(graphName, height)
-
-        pixmap = QPixmap(graphName)
+        self.resizeImage(graph1Name, height)
+        pixmap = QPixmap(graph1Name)
         self.graph1Main.setPixmap(pixmap)
 
         size = self.graph2Main.size()
         height = size.height()
-
-        pixmap = QPixmap(graphName)
+        self.resizeImage(graph2Name, height)
+        pixmap = QPixmap(graph2Name)
         self.graph2Main.setPixmap(pixmap)
 
 
     def drawTest(self):
-        graphName = "graphTest.png"
+        graph1Name = "graph1Test.png"
+        graph2Name = "graph2Test.png"
 
         N = 0
 
         try:
-            N = int(self.gridNumberTest.toPlainText())
+            N = int(self.gridNumberMain.toPlainText())
         except BaseException:
             return
 
-        x = [i for i in range(N)]
-        y = [i * i * i for i in x]
+        x, y, y2 = method.calculate("test", N)
+        diff = [abs(y[i] - y2[i]) for i in range(len(y))]
 
-        plt.subplot(111)
         plt.plot(x, y)
-        plt.savefig(graphName)
+        plt.savefig(graph1Name)
+        plt.clf()
+
+        plt.plot(x, diff)
+        plt.savefig(graph2Name)
         plt.clf()
 
         size = self.graph1Test.size()
         height = size.height()
-
-        self.resizeImage(graphName, height)
-
-        pixmap = QPixmap(graphName)
+        self.resizeImage(graph1Name, height)
+        pixmap = QPixmap(graph1Name)
         self.graph1Test.setPixmap(pixmap)
 
         size = self.graph2Test.size()
         height = size.height()
-
-        pixmap = QPixmap(graphName)
+        self.resizeImage(graph2Name, height)
+        pixmap = QPixmap(graph2Name)
         self.graph2Test.setPixmap(pixmap)
 
 
